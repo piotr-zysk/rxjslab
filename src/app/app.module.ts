c3816d0ae32b60415993ce0d8f17964b1c15bf5b
Piotr Zysk <pizy123@gmail.com>
2020-05-24 15:50:38 +0200

    typeahead base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TypeAheadComponent } from './type-ahead/type-ahead.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypeAheadComponent
   ],
   imports: [
     BrowserModule
